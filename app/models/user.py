7708b12b20f3e62751219c026169b9f04f7c514a
ffauzan <f.fauzan@outlook.com>
2025-06-08 15:52:08 +0700

    Add used_in_collaborative in users table

--- app/models/user.py
+++ app/models/user.py
@@ -8,6 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     onboarding_done = db.Column(db.Boolean, default=False, nullable=False)
+    used_in_collaborative = db.Column(db.Boolean, default=False, nullable=False)
     
     preferences = db.relationship('UserPreferences', back_populates='user', lazy='dynamic')
     
